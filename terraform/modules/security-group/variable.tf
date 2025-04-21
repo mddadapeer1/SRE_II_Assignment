480a370f2e448b37ab29078083ad7b196bd89653
Md Dadapeer <127332734+mddadapeer1@users.noreply.github.com>
2025-04-21 22:06:12 +0530

    Create variable.tf

---
+++ terraform/modules/security-group/variable.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+  default     = "Managed by Terraform"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the security group will be created"
+  type        = string
+  default     = ""
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
